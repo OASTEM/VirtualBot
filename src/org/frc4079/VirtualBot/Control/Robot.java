9e3b216d1d4e9fb954616f38a05ae7d2323c6f1b
Kevin Tran <KTOmega@users.noreply.github.com>
2017-07-04 03:09:16 -0700

    Annotate Robot.java
    Add units and more descriptions.

--- src/org/frc4079/VirtualBot/Control/Robot.java
+++ src/org/frc4079/VirtualBot/Control/Robot.java
@@ -3,11 +3,12 @@ package org.frc4079.VirtualBot.Control;
 public class Robot{
   private SpeedController left, right;
   public final double MAX_SPEED = 15; //feet per second
-  public final double WHEEL_CIR = 6 * Math.PI;
+  public final double WHEEL_CIR = 6 * Math.PI; // in inches
   public final double GEAR_RATIO = 7.3;
   public final double TP_REV = 80; //ticks per revolution
   public final double TIMER = 50;
   public final double SPEED_SCALE = 10;//
+  // units of SPEED_MOD is: ticks per revolution per millisecond
   private final double SPEED_MOD = MAX_SPEED / 1000 * 12 * GEAR_RATIO * TP_REV * TIMER / WHEEL_CIR;
   //private final double SPEED_MOD = 557.6;
   
@@ -37,6 +38,8 @@ public class Robot{
   public void updateVirtualMotors(double m){
     //double lMove = left.speed == 0 ? 0 : (left.speed * SPEED_MOD + (Math.random()*2) - 1);
     //double rMove = right.speed == 0 ? 0 : (right.speed * SPEED_MOD + (Math.random()*2) - 1);
+	// scale each wheel to a proportion of the speed of the wheel.
+	// limited to the maximum speed denoted by SPEED_MOD.
 	double lMove = left.speed == 0 ? 0 : (left.speed * SPEED_MOD);
 	double rMove = right.speed == 0 ? 0 : (right.speed * SPEED_MOD);
     left.distance += lMove;
